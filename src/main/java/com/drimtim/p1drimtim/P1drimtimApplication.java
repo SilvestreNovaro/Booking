9efc0bc046a7d8928fcc9449448ab51eeccd9a55
SilvestreNovaro <silvestrenovarohueyo@gmail.com>
2023-03-24 14:15:43 -0300

    Initial commit

---
+++ src/main/java/com/drimtim/p1drimtim/P1drimtimApplication.java
@@ -0,0 +1,13 @@
+package com.drimtim.p1drimtim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1drimtimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1drimtimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/drimtim/p1drimtim/P1drimtimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drimtim.p1drimtim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1drimtimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
